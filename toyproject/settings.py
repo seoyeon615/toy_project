67f92b08575cb82526c7f35250ef5db76d5b533f
kimgyurin <koong5111@naver.com>
2026-05-31 21:57:55 +0900

    서버 css 깨짐 해결 중

--- toyproject/settings.py
+++ toyproject/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"  # 로컬용 경로
 
 AUTH_USER_MODEL = 'users.User'
 
